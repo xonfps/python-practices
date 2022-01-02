e7f73c64af32a829d9eea6612e609f57de2d5096
Isaque Souza <isaque.souza@lunes.io>
2022-01-02 20:01:32 -0300

    add: a test of function for sum three numbers

--- test/test_sum.py
+++ test/test_sum.py
@@ -1,5 +1,10 @@
 from functions.sum import sum_two_numbers
+from functions.sum import sum_three_numbers
 
 
 def test_sum_two_numbers():
     assert sum_two_numbers(1,2) == 3
+
+
+def test_sum_three_numbers():
+    assert sum_three_numbers(1,1,1) == 3
