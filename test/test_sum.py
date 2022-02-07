39c8b91356936298f1703d6c8e422f708f3b44d7
Lucas Oliveira <lucas.oliveira@lunes.io>
2022-02-07 14:05:39 +0000

    refactored: functions of sum

--- test/test_sum.py
+++ test/test_sum.py
@@ -1,10 +1,9 @@
-from functions.sum import sum_two_numbers
-from functions.sum import sum_three_numbers
+from functions.sum import sum_two_numbers, sum_several_numbers
 
 
 def test_sum_two_numbers():
     assert sum_two_numbers(1,2) == 3
 
 
-def test_sum_three_numbers():
-    assert sum_three_numbers(1,1,1) == 3
+def test_sum_several_numbers():
+    assert sum_several_numbers(1,1,1) == 3
